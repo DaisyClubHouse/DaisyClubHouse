b21c0c358ec5897f657a7e3a31caf699a5ab3b51
付建邦 <fujianbang0ops@gmail.com>
2022-07-30 00:44:35 +0800

    add grid

--- platform/src/components/Goband.vue
+++ platform/src/components/Goband.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goband">
-    <canvas id="chessboard" width="500" height="500">
+    <canvas id="chessboard">
       <p>你的浏览器不支持Canvas</p>
     </canvas>
   </div>
@@ -9,15 +9,32 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 
+const space: number = 50; // 棋盘件空格
+const scale: number = 10; // 棋盘格数 scale x scale
+const margin: number = 50; // 棋盘外空白
+
 onMounted(() => {
-  const canvas: HTMLCanvasElement = document.getElementById(
-    "canvas"
+  let canvas: HTMLCanvasElement = document.getElementById(
+    "chessboard"
   ) as HTMLCanvasElement;
+  canvas.width = 2 * margin + space * scale;
+  canvas.height = 2 * margin + space * scale;
 
   let ctx = canvas.getContext("2d");
   if (ctx != null) {
-    ctx.fillStyle = "rgb(200,0,0)";
-    ctx.fillRect(10, 10, 55, 50);
+    // 绘制网格
+    let grid = new Path2D();
+    for (let i = 0; i < scale + 1; i++) {
+      // 横
+      grid.moveTo(margin, margin + i * space);
+      grid.lineTo(margin + space * scale, margin + i * space);
+      // 纵
+      grid.moveTo(margin + i * space, margin);
+      grid.lineTo(margin + i * space, margin + space * scale);
+    }
+
+    ctx.strokeStyle = "black";
+    ctx.stroke(grid);
   }
 });
 </script>
@@ -26,7 +43,7 @@ onMounted(() => {
 @boardBg: #ffd75b;
 
 .goband {
-  border: 1px solid green;
+  // border: 1px solid green;
   #chessboard {
     background-color: @boardBg;
   }
