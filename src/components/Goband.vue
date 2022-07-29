ce6bd452b39b47c3e996644cfb53e3c1735fede5
付建邦 <fujianbang0ops@gmail.com>
2022-07-30 02:08:19 +0800

    完善落子计算和绘制

--- src/components/Goband.vue
+++ src/components/Goband.vue
@@ -9,23 +9,35 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 
+enum PlayerRole {
+  White,
+  Black,
+}
+
+// 棋子坐标
+interface PieceCoord {
+  x: number;
+  y: number;
+}
+
 const space: number = 50; // 棋盘件空格
 const scale: number = 10; // 棋盘格数 scale x scale
 const margin: number = 50; // 棋盘外空白
+const pieceSize: number = 18; // 棋子大小（直径）
 
-onMounted(() => {
-  let canvas: HTMLCanvasElement = document.getElementById(
-    "chessboard"
-  ) as HTMLCanvasElement;
+// 棋盘画布对象
+let chessCanvas: HTMLCanvasElement;
 
-  let ctx = canvas.getContext("2d");
-  if (ctx == null) {
-    console.error("获取画布失败");
-    return;
-  }
+// 棋盘画布操作对象
+let chessboardCtx: CanvasRenderingContext2D;
+// 棋子画布操作对象
+let piecesCtx: CanvasRenderingContext2D;
+
+// 构建棋盘
+function drawChessboard() {
   // 设置棋盘大小
-  canvas.width = 2 * margin + space * scale;
-  canvas.height = 2 * margin + space * scale;
+  chessCanvas.width = 2 * margin + space * scale;
+  chessCanvas.height = 2 * margin + space * scale;
 
   // 绘制网格
   let grid = new Path2D();
@@ -40,8 +52,73 @@ onMounted(() => {
     grid.lineTo(x, y);
   }
 
-  ctx.strokeStyle = "black";
-  ctx.stroke(grid);
+  chessboardCtx.strokeStyle = "black";
+  chessboardCtx.stroke(grid);
+
+  // 事件处理
+  chessCanvas.onclick = bindBoardEvent;
+}
+
+let counter = 0;
+// 获取玩家持方
+function getPlayerRole(): PlayerRole {
+  counter++;
+  if (counter % 2 == 1) {
+    return PlayerRole.Black;
+  }
+  return PlayerRole.White;
+}
+
+// 绑定棋盘事件
+function bindBoardEvent(e: MouseEvent) {
+  console.log("绑定棋盘事件");
+  // console.log(e);
+  console.log(e.offsetX, e.offsetY);
+
+  // 棋盘定位转换为坐标系坐标，以棋盘左上角为(0, 0）
+  const x = Math.floor((e.offsetX + space / 2 - margin) / space);
+  const y = Math.floor((e.offsetY + space / 2 - margin) / space);
+
+  console.log(x, y);
+  const point: PieceCoord = { x, y };
+
+  // 判断执方
+  drawPiece(point, getPlayerRole());
+}
+
+// 画棋子
+function drawPiece(point: PieceCoord, role: PlayerRole) {
+  // 坐标系转换为实际棋子位置
+  const x = point.x * space + margin;
+  const y = point.y * space + margin;
+
+  let ctx = piecesCtx;
+  ctx.beginPath();
+  ctx.arc(x, y, pieceSize, 0, 2 * Math.PI);
+  ctx.closePath();
+
+  if (role == PlayerRole.White) {
+    ctx.fillStyle = "black";
+  } else {
+    ctx.fillStyle = "white";
+  }
+  ctx.fill();
+}
+
+onMounted(() => {
+  // 初始化棋盘画布
+  chessCanvas = document.getElementById("chessboard") as HTMLCanvasElement;
+
+  const ctx = chessCanvas.getContext("2d");
+  if (ctx == null) {
+    console.error("init failed");
+    return;
+  }
+  chessboardCtx = ctx;
+  piecesCtx = ctx;
+
+  // 获取并初始化棋盘
+  drawChessboard();
 });
 </script>
 
