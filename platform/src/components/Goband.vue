7b03e037eb31ae837ae12427d8e95800f4fc8e83
付建邦 <fujianbang0ops@gmail.com>
2022-07-29 22:40:55 +0800

    delete basic structure

---
+++ platform/src/components/Goband.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="chessboard">
+    <h1>五子棋</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.chessboard {
+  border: 1px solid red;
+}
+</style>
--- platform/src/views/HomeView.vue
+++ platform/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Goband from "@/components/Goband.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Goband />
   </main>
 </template>
