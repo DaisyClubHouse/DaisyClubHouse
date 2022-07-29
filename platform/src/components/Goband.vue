6e11d2b1ef8f46016352ef152aff1112bd532271
付建邦 <fujianbang0ops@gmail.com>
2022-07-30 00:00:34 +0800

    基本canvas

--- platform/src/components/Goband.vue
+++ platform/src/components/Goband.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="chessboard">
-    <h1>五子棋</h1>
+  <div class="goband">
+    <canvas id="chessboard" width="500" height="500">
+      <p>你的浏览器不支持Canvas</p>
+    </canvas>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
 
-<style scoped>
-.chessboard {
-  border: 1px solid red;
+onMounted(() => {
+  const canvas: HTMLCanvasElement = document.getElementById(
+    "canvas"
+  ) as HTMLCanvasElement;
+
+  let ctx = canvas.getContext("2d");
+  if (ctx != null) {
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(10, 10, 55, 50);
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@boardBg: #ffd75b;
+
+.goband {
+  border: 1px solid green;
+  #chessboard {
+    background-color: @boardBg;
+  }
 }
 </style>
--- platform/src/views/HomeView.vue
+++ platform/src/views/HomeView.vue
@@ -3,7 +3,20 @@ import Goband from "@/components/Goband.vue";
 </script>
 
 <template>
-  <main>
+  <div class="goband-wrapper">
+    <span class="title">五子棋</span>
     <Goband />
-  </main>
+  </div>
 </template>
+
+<style lang="less" scoped>
+.goband-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .title {
+    font-size: 20px;
+    text-align: center;
+  }
+}
+</style>
